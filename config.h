059af0689e0df95ad1c900226700d9924be761b6
tetr-a <tetr-a@tetr.com>
2024-08-15 01:23:20 +0700

    15agustus

--- config.h
+++ config.h
@@ -13,7 +13,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#ffffff", "#000000" },
-	[SchemeSel] = { "#ff0cd2", "#000000" },
+	[SchemeSel] = { "#ff00ff", "#000000" },
 	[SchemeOut] = { "#000000", "#ffffff" },
 };
 static const unsigned int alphas[SchemeLast][2] = {
